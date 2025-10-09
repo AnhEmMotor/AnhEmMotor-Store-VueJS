2ebc095686c4aebac1d63ff31b5aa82cf6da1bf0
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2025-10-09 08:38:17 +0700

    Finish promotion front-end page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AboutUs from '@/views/AboutUs.vue'
 import NewsView from '@/views/NewsView.vue'
+import PromotionPage from '@/views/PromotionPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       name: 'news',
       path: '/news',
       component: NewsView,
+    },
+    {
+      name: 'promotion',
+      path: '/promotion',
+      component: PromotionPage,
     },
   ],
 })
