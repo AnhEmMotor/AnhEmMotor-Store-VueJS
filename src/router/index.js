a9f99b49d806defb01aeea4e8c52984190e1a26a
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2025-10-17 15:14:51 +0700

    Remove HelloWorld component and update router to include AccessoriesView

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,11 @@ import AboutUs from '@/views/AboutUs.vue'
 import NewsView from '@/views/NewsView.vue'
 import PromotionPage from '@/views/PromotionPage.vue'
 import ServicePage from '@/views/ServicePage.vue'
+<<<<<<< HEAD
 import TheIndex from '@/views/TheIndex.vue'
+=======
+import AccessoriesView from '@/components/accessories/AccessoriesView.vue'
+>>>>>>> 41cdf69 (Remove HelloWorld component and update router to include AccessoriesView)
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +36,11 @@ const router = createRouter({
       name: 'service',
       path: '/service',
       component: ServicePage,
+    },
+    {
+      name: 'list-category',
+      path: '/listings',
+      component: AccessoriesView,
     },
   ],
 })
