7f0d990eefadf423b46dcd334d47cfe6ad6ed59c
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2025-10-17 15:20:20 +0700

    fix some error in router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,8 @@ import AboutUs from '@/views/AboutUs.vue'
 import NewsView from '@/views/NewsView.vue'
 import PromotionPage from '@/views/PromotionPage.vue'
 import ServicePage from '@/views/ServicePage.vue'
-<<<<<<< HEAD
 import TheIndex from '@/views/TheIndex.vue'
-=======
 import AccessoriesView from '@/components/accessories/AccessoriesView.vue'
->>>>>>> 41cdf69 (Remove HelloWorld component and update router to include AccessoriesView)
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
